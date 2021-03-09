9699e906734b2c4e6ab32a44a4bce52eef81ebed
longfangsong <longfangsong@icloud.com>
2021-03-09 13:39:01 +0000

    Deploying to gh-pages from  @ 48cc4a48becd7d4dda488491fe7497cdf2e67d7c 🚀

---
+++ static/shared.js
@@ -0,0 +1,26 @@
+window.addEventListener("load", function () {
+    if (window.innerWidth > 420) {
+        $("#expand-button").classList.add("expanded");
+    }
+    else {
+        $("#expand-button").classList.add("collapsed");
+    }
+    $("#expand-button").onclick = function () {
+        if ($("#expand-button").classList.contains("expanded")) {
+            $("#expand-button").classList.remove("expanded");
+            $("#expand-button").classList.add("collapsed");
+            $(".side-bar").classList.remove("expanded");
+            $(".side-bar").classList.add("collapsed");
+            $(".header-bar").classList.remove("collapsed");
+            $(".header-bar").classList.add("expanded");
+        }
+        else {
+            $("#expand-button").classList.remove("collapsed");
+            $("#expand-button").classList.add("expanded");
+            $(".side-bar").classList.remove("collapsed");
+            $(".side-bar").classList.add("expanded");
+            $(".header-bar").classList.remove("expanded");
+            $(".header-bar").classList.add("collapsed");
+        }
+    };
+});
