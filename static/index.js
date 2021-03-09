a3b974b02a86dee6380c8a3890b9a2b10b4d8933
longfangsong <longfangsong@icloud.com>
2021-03-09 06:24:13 +0000

    Deploying to gh-pages from  @ 190e0815ec1432e2ef6414bac44733beed30f7ce 🚀

--- static/index.js
+++ static/index.js
@@ -7,9 +7,10 @@ window.addEventListener("load", function () {
     var selected_how = how[Math.floor(Math.random() * how.length)];
     var selected_why = why[Math.floor(Math.random() * why.length)];
     $("#do-you-know-what").innerHTML = selected_what.name;
-    $("#do-you-know-what-button").onclick = function () { return window.location.href = "/" + language + "/" + selected_what.section + "/" + selected_what.filename + ".html"; };
+    var public_url = $("meta[name='public-url']").getAttribute("content");
+    $("#do-you-know-what-button").onclick = function () { return window.location.href = public_url + "/" + language + "/" + selected_what.section + "/" + selected_what.filename + ".html"; };
     $("#do-you-know-how").innerHTML = selected_how.name;
-    $("#do-you-know-how-button").onclick = function () { return window.location.href = "/" + language + "/" + selected_how.section + "/" + selected_how.filename + ".html"; };
+    $("#do-you-know-how-button").onclick = function () { return window.location.href = public_url + "/" + language + "/" + selected_how.section + "/" + selected_how.filename + ".html"; };
     $("#do-you-know-why").innerHTML = selected_why.name;
-    $("#do-you-know-why-button").onclick = function () { return window.location.href = "/" + language + "/" + selected_why.section + "/" + selected_how.filename + ".html"; };
+    $("#do-you-know-why-button").onclick = function () { return window.location.href = public_url + "/" + language + "/" + selected_why.section + "/" + selected_how.filename + ".html"; };
 });
--- static/random_page.js
+++ static/random_page.js
@@ -1,7 +1,8 @@
 window.addEventListener("load", function () {
     $("#random-page").onclick = function () {
+        var public_url = $("meta[name='public-url']").getAttribute("content");
         var language = navigator.language.split("-")[0];
         var to_visit = site_index[Math.floor(Math.random() * site_index.length)];
-        window.location.href = "/" + language + "/" + to_visit.section + "/" + to_visit.filename + ".html";
+        window.location.href = public_url + "/" + language + "/" + to_visit.section + "/" + to_visit.filename + ".html";
     };
 });
